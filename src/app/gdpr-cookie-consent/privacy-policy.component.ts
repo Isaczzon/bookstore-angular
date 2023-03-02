711019927c5c44305e46144580e6097397bf3e8e
ISAC\isacg <isac@webking.se>
2023-03-02 11:22:08 +0100

    Revert "Aspnet-core - Initial commit"
    This reverts commit 61b595de5030875c21652f2b8130907839949065.

--- src/app/gdpr-cookie-consent/privacy-policy.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'abp-privacy-policy',
-  templateUrl: './privacy-policy.component.html',
-})
-export class PrivacyPolicyComponent {
-  constructor() {}
-}
